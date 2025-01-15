9b9b08c34a48015a31bdf61a551a3307077a9bbb
sersang <sersang01@gmail.com>
2025-01-15 19:23:58 +0600

    Changed nav bar

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const textElement = document.querySelector('.sliding-text');
     textElement.classList.add('slide-in');
 });
+
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
